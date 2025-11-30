afbf73d3b4cdcf79b3ae9385f9a5993d2b8b2db9
bbangddeok2 <songseungil39@gmail.com>
2025-11-30 15:36:40 +0900

    햄버거 수정중

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 $(document).ready(function () {
   $(".ham_icon i").click(function () {
     $(".mobile_menu").addClass("active");
+    $(".ham_icon").addClass("hide"); // ← 햄버거 아이콘 숨기기
   });
 
   $(".mobile_menu i.fa-xmark").click(function () {
     $(".mobile_menu").removeClass("active");
+    $(".ham_icon").removeClass("hide"); // ← 다시 보이기
   });
 
   $(".mobile_main").click(function () {
